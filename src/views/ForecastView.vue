2e12a532dd8ff955e1c76701be5dda013d505e33
ig7oo <davidwesterlund012@gmail.com>
2024-10-16 09:14:05 +0300

    Prognossida klar

--- src/views/ForecastView.vue
+++ src/views/ForecastView.vue
@@ -27,5 +27,3 @@ onMounted(() => {
     margin: 0 1em;
 }
 </style>
-
-https://api.open-meteo.com/v1/forecast?latitude=52.52&longitude=13.41&daily=weather_code,temperature_2m_max,temperature_2m_min,precipitation_sum,precipitation_probability_max,wind_speed_10m_max,wind_gusts_10m_max,wind_direction_10m_dominant&wind_speed_unit=ms&timezone=auto
